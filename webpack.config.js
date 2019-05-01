848f48e6e805ba408d49291bf32a0feed8e8a1f0
Unknown <s110619046@stu.ntue.edu.tw>
2019-05-02 01:17:08 +0800

    第一次commit
    用老師的wk9第一課範例做出來了

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+let path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let htmlPages = ['index'];
+
+let htmlPlugins = htmlPages.map(page => 
+    new HtmlWebpackPlugin({
+       filename: `${page}.html`,
+       template: `assets/${page}.html`,
+    })
+);
+
+module.exports = {
+    entry: {
+        polyfill: "babel-polyfill",
+        bundle: "./assets/js/app.js"
+    },//'./assets/js/app.js',
+    output:{
+        path: path.join(__dirname, "./dist"),
+        filename: "[name].[chunkhash].js"
+        // path: path.join(__dirname, './dist'),
+        // filename: 'bundle.[chunkhash].js',
+        // publicPath: './dist/'
+    },
+    module:{
+        rules:[{
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use:[{
+                loader: 'url-loader',
+                options:{
+                    limit: 100000,
+                    outputPath: './images/'
+                }
+            },
+            'image-webpack-loader'
+            ]
+        },
+
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+               use: [{
+                  loader: 'css-loader',
+                  options: {
+                     url: false
+                  }
+               }, {
+                  loader: 'postcss-loader'
+               }]
+            })
+         },
+         {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            use: {
+               loader: 'file-loader',
+               options: {
+                  outputPath: 'css/fonts',
+                  name: '[name].[ext]',
+               },
+            }
+        },
+        {
+            test: /\.(js)$/,
+            exclude: /(node_modules)/,
+            use:{
+                loader: 'babel-loader',
+                options:{
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('css/style.[hash].css'),
+        ...htmlPlugins,
+        new HtmlWebpackPlugin({
+            template:'assets/index.html'
+        })
+    ],
+    devServer: {
+       contentBase: 'dist'
+    }
+} 
